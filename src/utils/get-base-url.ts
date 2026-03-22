d7b8d98604b1acd4a9f25a08558831dde31c73fc
Tatiane Weitzel <weitzeltati@gmail.com>
2026-03-22 17:08:48 -0300

    build: ajustes get-base-url

--- src/utils/get-base-url.ts
+++ src/utils/get-base-url.ts
@@ -1,9 +1,5 @@
 export const getBaseUrl = () => {
-    const baseUrl = process.env.API_URL ?? process.env.APP_URL
-
-    if (!baseUrl) {
-        console.warn("⚠️ API_URL/BASE_URL não definida, usando localhost")
-    }
-
-    return baseUrl ?? `http://localhost:${process.env.PORT || "8080"}`;
+    return (
+        process.env.API_URL || process.env.APP_URL || `http://localhost:${process.env.PORT || 4000}`
+    )
 }
