75dbbee77290f05d08a8f7934ed99acd9833079f
Tatiane Weitzel <weitzeltati@gmail.com>
2026-03-22 12:28:10 -0300

    build: static public

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 //app.use("/webhook/stripe", express.raw({ type: "application/json" }));
 app.use(express.json());
-app.use(express.static("public"));
+//app.use(express.static("public"));
 app.use(cookieParser())
 //app.use(routes);
 app.use(errorHandler);
